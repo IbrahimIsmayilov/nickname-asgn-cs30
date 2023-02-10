308bb3a116c18e85b775ab8222ebf36ef6e3db39
IbrahimIsmayilov <90726322+IbrahimIsmayilov@users.noreply.github.com>
2023-02-10 11:23:23 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -14,9 +14,15 @@ names_and_nicknames = {
 }
 
 def random_nickname():
-    random_index = random.randint(0, len(names_and_nicknames["nicknames"]))
+    random_index = random.randint(0, len(names_and_nicknames["nicknames"]) - 1)
     random_nickname = names_and_nicknames["nicknames"][random_index]
     return random_nickname
+
+def check_nickname(added_new_nickname):
+    for i in range(len(names_and_nicknames["nicknames"]) - 1):
+        if names_and_nicknames["nicknames"][i] == added_new_nickname:
+                return i
+    return -1
 
 # Menu Loop
 loop = True
@@ -28,35 +34,45 @@ while loop:
     print("2. Display a Random Nickname")
     print("3. Display All Nicknames")
     print("4. Add a Nickname")
-    print("5. Remove a Nickname")
-    print("6. Exit")
+    print("5. Edit a Nickname")
+    print("6. Remove a Nickname")
+    print("7. Exit")
 
     # Get Menu Selection From The User
     selection = input("Enter Selection (1-6): ")
 
     # Action Based on Menu Selection
     if selection == "1":
-        print("\nChange Name")
-        first_name = ("Please enter a first name:")
-        last_name = ("Please enter a last name:")
+        print("\nCHANGE NAME")
+        first_name = input("Please enter a first name:")
+        last_name = input("Please enter a last name:")
         print(f"\nCurrent Name is {first_name} {last_name}.")
     elif selection == "2":
-        print("\nDisplay a Random Nicknname")
+        print("\nRANDOM NICKNAME")
         nickname = random_nickname()
-        print(f'{first_name} "{nickname}" {last_name}') 
+        print(f'\n{first_name} "{nickname}" {last_name}') 
     elif selection == "3":
-        print("\nDisplay All Nicknnames")
-        for name in names_and_nicknames["nicknames"]:
-            print(f'{first_name} "{name} ')
+        print("\nALL NICKNAMES")
+        for nickname in names_and_nicknames["nicknames"]:
+            print(f'{first_name} "{nickname}" {last_name}') 
     elif selection == "4":
-        print(grades_list)
-        for i in range(len(grades_list)):
-            grades_list[i] = random.randint(1, 99)
-        print(grades_list)
-        print("GRADES HAVE BEEN RANDOMIZED")
-    elif selection == "4":
-        pass
+        print("\nADD A NICKNAME")
+        new_nickname = input("Please enter a nickname to add:")
+        index = check_nickname(new_nickname)
+        if (index == -1):
+            names_and_nicknames["nicknames"].append(new_nickname)
+            print(f"{new_nickname} is added to the nickname list.")
+        else: 
+            print(f"{new_nickname} is already in the list.")
     elif selection == "5":
-        pass
-    elif selection == "6":
-        loop = False
+        print("\nEDIT A NICKNAME")
+        edit_nickname = input("Please enter a nickname to edit:")
+        new_nickname = input("Please enter the new nickanem to add:")
+        index = check_nickname(edit_nickname)
+        if index == -1:
+            print(f"{edit_nickname} was not found in the nickname list")
+        else:
+            names_and_nicknames["nicknames"][index] == new_nickname
+            
+        
+
